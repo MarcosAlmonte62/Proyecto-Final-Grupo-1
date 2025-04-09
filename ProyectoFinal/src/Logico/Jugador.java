50ebe523d3486ebadcf6b7e956d4e75c200ef126
EliteBook <EliteBook@DESKTOP-0QKARMP>
2025-04-09 13:28:53 -0400

    Ajustando setters de Jugador

--- ProyectoFinal/src/Logico/Jugador.java
+++ ProyectoFinal/src/Logico/Jugador.java
@@ -39,12 +39,24 @@ public class Jugador implements Serializable {
         return nombre;
     }
 
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
     public int getEdad() {
         return edad;
     }
 
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
     public String getNacionalidad() {
         return nacionalidad;
+    }
+
+    public void setNacionalidad(String nacionalidad) {
+        this.nacionalidad = nacionalidad;
     }
 
     public Lesion getLesion() {
